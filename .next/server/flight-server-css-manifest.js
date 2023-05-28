b3e32c82be20d9faaf51859ba91436b697e320ff
Dana Izadpanah <danaizad8624@gmail.com>
2023-05-28 14:54:06 -0500

    I changed a word that is it

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,20 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/danaizadpanah/Documents/vscode_inifinity_corp/infinity-corp/app/layout.tsx": [
+      "/Users/danaizadpanah/Documents/vscode_inifinity_corp/infinity-corp/app/css/style.css",
+      "/Users/danaizadpanah/Documents/vscode_inifinity_corp/infinity-corp/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\",\"display\":\"swap\"}],\"variableName\":\"inter\"}",
+      "/Users/danaizadpanah/Documents/vscode_inifinity_corp/infinity-corp/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Architects_Daughter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-architects-daughter\",\"weight\":\"400\",\"display\":\"swap\"}],\"variableName\":\"architects_daughter\"}"
+    ],
+    "/Users/danaizadpanah/Documents/vscode_inifinity_corp/infinity-corp/app/(default)/layout.tsx": [
+      "/Users/danaizadpanah/Documents/vscode_inifinity_corp/infinity-corp/node_modules/aos/dist/aos.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/danaizadpanah/Documents/vscode_inifinity_corp/infinity-corp/app/(default)/page": [
+      "/Users/danaizadpanah/Documents/vscode_inifinity_corp/infinity-corp/app/css/style.css",
+      "/Users/danaizadpanah/Documents/vscode_inifinity_corp/infinity-corp/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\",\"display\":\"swap\"}],\"variableName\":\"inter\"}",
+      "/Users/danaizadpanah/Documents/vscode_inifinity_corp/infinity-corp/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Architects_Daughter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-architects-daughter\",\"weight\":\"400\",\"display\":\"swap\"}],\"variableName\":\"architects_daughter\"}",
+      "/Users/danaizadpanah/Documents/vscode_inifinity_corp/infinity-corp/node_modules/aos/dist/aos.css"
+    ]
+  }
+}
